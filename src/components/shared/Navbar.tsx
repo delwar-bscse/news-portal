a5ee731c88f78e9ff75954db71bad4a9c792c3d5
Md. Delwar Hossain <>
2025-02-19 21:20:26 +0600

    menu icon

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { AiOutlineMenu } from "react-icons/ai";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -9,6 +10,8 @@ import {
   NavigationMenuTrigger,
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
+import { Switch } from "@/components/ui/switch";
+import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
   return (
@@ -22,39 +25,43 @@ const Navbar = () => {
         {/* Desktop Menu */}
         <NavigationMenu className="hidden lg:flex">
           <NavigationMenuList>
-            <NavigationMenuItem className="flex items-center gap-2">
+            <NavigationMenuItem className="flex items-center gap-4">
               <NavigationMenuLink href="/news" className="hover:text-red-500">
                 News
               </NavigationMenuLink>
-              <NavigationMenuTrigger className="hover:text-red-500">Services</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="text-gray-600 shadow-md px-5 py-4 space-y-2">
-                  <li>
-                    <NavigationMenuLink
-                      href="/services/web"
-                      className="hover:text-red-500"
-                    >
-                      Web Development
-                    </NavigationMenuLink>
-                  </li>
-                  <li>
-                    <NavigationMenuLink
-                      href="/services/apps"
-                      className="hover:text-red-500"
-                    >
-                      Mobile App
-                    </NavigationMenuLink>
-                  </li>
-                  <li>
-                    <NavigationMenuLink
-                      href="/services/seo"
-                      className="hover:text-red-500"
-                    >
-                      SEO
-                    </NavigationMenuLink>
-                  </li>
-                </ul>
-              </NavigationMenuContent>
+              <NavigationMenuLink className="hover:text-red-500">
+                <NavigationMenuTrigger className="hover:text-red-500">
+                  Services
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="text-gray-600 shadow-md px-5 py-4 space-y-2">
+                    <li>
+                      <NavigationMenuLink
+                        href="/services/web"
+                        className="hover:text-red-500"
+                      >
+                        Web Development
+                      </NavigationMenuLink>
+                    </li>
+                    <li>
+                      <NavigationMenuLink
+                        href="/services/apps"
+                        className="hover:text-red-500"
+                      >
+                        Mobile App
+                      </NavigationMenuLink>
+                    </li>
+                    <li>
+                      <NavigationMenuLink
+                        href="/services/seo"
+                        className="hover:text-red-500"
+                      >
+                        SEO
+                      </NavigationMenuLink>
+                    </li>
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuLink>
               <NavigationMenuLink href="/about" className="hover:text-red-500">
                 About
               </NavigationMenuLink>
@@ -69,6 +76,20 @@ const Navbar = () => {
         </NavigationMenu>
 
         {/* Color switcher and Login button */}
+        <div className="hidden lg:flex items-center gap-4">
+          <div className="flex items-center gap-1">
+            <span>Dark Mode</span>
+            <Switch />
+          </div>
+          <Button variant="default">Button</Button>
+        </div>
+
+        {/* Mobile Menu */}
+        <div className="lg:hidden">
+          <Button variant={"outline"}>
+            <AiOutlineMenu />
+          </Button>
+        </div>
       </nav>
     </header>
   );
